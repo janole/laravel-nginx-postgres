f6714b735636684798c0c9941891dd767ce3f834
Jan Ole Suhr <ole@mobileways.de>
2019-10-29 20:56:08 +0100

    Adapt the build script to github workflows (#wip 1)

--- build.sh
+++ build.sh
@@ -1,27 +1,41 @@
 #!/bin/sh
 set -e
 
+#
 IMAGE=janole/laravel-nginx-postgres
-VERSION=1.0.7
+VERSION=`cat version`
 
-if git fetch && test "`git rev-parse --abbrev-ref HEAD`" = "master" && git diff-index --quiet HEAD; then
-
-    TARGET="${IMAGE}:${VERSION}"
-
+# Branch or Tag ...
+if [ -n ${GITHUB_REF} ]; then
+    BRANCH=`echo ${GITHUB_REF} | sed 's=.*/=/='`;
 else
-
-    TARGET="${IMAGE}:${VERSION}-dev"
-
+    BRANCH=`(git rev-parse --abbrev-ref HEAD 2>/dev/null) | grep -v "^master$" | sed 's=^=/='`;
 fi
 
+IMAGE=${IMAGE}${BRANCH}${COUNT}
+
+COUNT=`git rev-list HEAD --count 2>/dev/null`
+VERSION=${VERSION}.${COUNT}
+
+# Create hierarchical versions (1.2.3 => "1.2" and "1")
+VERSION1=`sed "s/\(^[0-9]*\.[0-9]*\).*/\1/" version`
+VERSION0=`sed "s/\(^[0-9]*\).*/\1/" version`
+
+#
+TARGET=${IMAGE}:${VERSION}
+
+#
 echo "*** Build ${TARGET} based on master ..."
 
-docker build --pull -t "${TARGET}" .
+docker build --pull -t "${TARGET}" -t "${IMAGE}:${VERSION1}" -t "${IMAGE}:${VERSION0}" -t "${IMAGE}:latest" .
 
+#
 echo "*** Build images dependent on ${TARGET} ..."
 
-docker build -f unoconv/Dockerfile --build-arg "FROM=${TARGET}" -t "${TARGET}-unoconv" .
+docker build -f unoconv/Dockerfile --build-arg "FROM=${TARGET}" -t "${IMAGE}/unoconv:${VERSION}" -t "${IMAGE}/unoconv:${VERSION1}" -t "${IMAGE}/unoconv:${VERSION0}" -t "${IMAGE}/unoconv:latest" .
 
 echo "*** Push images ..."
 
-docker push "${TARGET}" && docker push "${TARGET}-unoconv"
+docker push "${TARGET}" 
+
+docker push "${IMAGE}/unoconv:${VERSION}"
